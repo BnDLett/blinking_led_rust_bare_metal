f9285a669f7e7136c0551a13c924f5541f238265
Lett <lett@ubuntu-23.10-desktop>
2024-05-31 22:15:00 -0400

    impl3d

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+//3F20_0008 fsel2 1<<3 turn pin21 into out
+//3F20_001c gpio1 set 1<<21 turns on pin21
+//3F20_0028 gpio1 set 1<<21 turns off pin21
+
+use core::{panic::PanicInfo, ptr};
+use core::arch::asm;
+
+mod boot {
+    use core::arch::global_asm;
+
+    global_asm!(
+        ".section .text._start"
+    );
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop{}
+    unsafe {
+        // turn pin 21 into an output
+        core::ptr::write_volatile(0x3F20_0008 as *mut u32, 1<<3);
+
+        loop {
+            // Turn pin on
+            core::ptr::write_volatile(0x3F20_001c as *mut u32, 1<<21);
+
+            for _ in 1..50000 {
+                asm!("nop");
+            }
+
+            // Turn pin off
+            core::ptr::write_volatile(0x3F20_0028 as *mut u32, 1<<21);
+        }
+    }
 }
 
 #[panic_handler]
 fn panic (_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
 }
